8dc585e9e0b124c397598708d62403158bc13bb7
Duy Chuong Vu <vu.duy.chuong@framgia.com>
2018-08-18 19:55:30 +0700

    Add check app in background

--- app/src/main/java/com/chuongvd/app/signal/BasicApp.java
+++ app/src/main/java/com/chuongvd/app/signal/BasicApp.java
@@ -1,38 +1,64 @@
-/*
- * Copyright 2017, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.chuongvd.app.signal;
 
+import android.app.Activity;
 import android.app.Application;
-import android.content.Context;
+import android.os.Bundle;
 
 /**
  * Android Application class. Used for accessing singletons.
  */
-public class BasicApp extends Application {
+public class BasicApp extends Application implements Application.ActivityLifecycleCallbacks {
 
     private static Application sSelf;
+    private static int sNumActivityStarted;
 
     @Override
     public void onCreate() {
         super.onCreate();
         sSelf = this;
+        registerActivityLifecycleCallbacks(this);
     }
 
     public static Application self() {
         return sSelf;
+    }
+
+    public static boolean isAppInBackground() {
+        return sNumActivityStarted <= 0;
+    }
+
+    @Override
+    public void onActivityCreated(Activity activity, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onActivityStarted(Activity activity) {
+        sNumActivityStarted++;
+    }
+
+    @Override
+    public void onActivityResumed(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityPaused(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityStopped(Activity activity) {
+        sNumActivityStarted--;
+    }
+
+    @Override
+    public void onActivitySaveInstanceState(Activity activity, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onActivityDestroyed(Activity activity) {
+
     }
 }
